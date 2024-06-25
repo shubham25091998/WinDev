e78ea73114c6c28159316c1b4de3bfcab8ea6d99
Shubham <46642598+shubham25091998@users.noreply.github.com>
2024-06-25 23:16:48 +0530

    Update Window.c

--- 01_Win32/01_Windowing/01_FirstWindow/Window.c
+++ 01_Win32/01_Windowing/01_FirstWindow/Window.c
@@ -26,13 +26,25 @@ LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 	WINAPI is callback convention
 
 	HINSTANCE -> handle to process instance
-					typdef of HANDLE-> typedef of PVOID -> typedef of void ptr
+					typdef of HANDLE-> typedef of PVOID -> typedef of void*
 
 	LPSTR -> long pointer to string
 			it is char** / wchar**
+
+	LpszCmdLine : it means long pointer to zero terminated (i.e. \0) string command line argument
 */
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR LpszCmdLine, int iCmdShow) {
-	
+	/*
+	*	Hungarian Notation rules : 
+	*				initial letter should be small and should denote variable type
+	*				Next letter should be capital
+	*				Afterward all letters should be small
+	*				Every new word in variable should start with capital letter
+	*				if pointer, then begin with 'p' and follow form rule 1
+	*				if global variable then start with 'g' and follow from rule 1
+	* 
+	*/
+
 	// Variable Declarations
 	/*
 		WNDCLASSEX -> window class extended -> it is a structure 
@@ -50,17 +62,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR LpszCmdLi
 
 	// Initializing Window Class
 	wndclass.cbSize = sizeof(WNDCLASSEX);
-	wndclass.style = CS_HREDRAW | CS_VREDRAW;		// CS_HREDRAW & CS_VREDRAW are macros; Words containing underscrore are macros
-	wndclass.cbClsExtra = 0;
-	wndclass.cbWndExtra = 0;
-	wndclass.lpfnWndProc = WndProc;
-	wndclass.lpszClassName = szClassName;
-	wndclass.lpszMenuName = NULL;
-	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	// Win32 API : GDI32 library
-	wndclass.hInstance = hInstance;
-	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);		// Win32 API
-	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);	// Win32 API
-	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;								// CS_HREDRAW & CS_VREDRAW are macros; Words containing underscrore are macros; CS -> class style
+	wndclass.cbClsExtra = 0;												// Extra info about class
+	wndclass.cbWndExtra = 0;												// extra info about window
+	wndclass.lpfnWndProc = WndProc;											// long pointer to function to window proceduure
+	wndclass.lpszClassName = szClassName;									// long pointer to string termineted with zero
+	wndclass.lpszMenuName = NULL;											// long pointer to zero terminated string = menu name
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);			// Win32 API : GDI32 library // handle to brush to background
+	wndclass.hInstance = hInstance;											
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);							// Win32 API // handle to cursor IDC : identifier of cursor, ARROW of arrow
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);						// Win32 API // handle to icon	IDI : identifier of icon, APPLICATION of application
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);						// handle to small icon
 
 	// Register The Above Window Class
 	RegisterClassEx(&wndclass);
@@ -68,8 +80,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR LpszCmdLi
 	// Create The Window In Memory
 	hwnd = CreateWindow(szClassName,
 		TEXT("This is new window!"),
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
+		WS_OVERLAPPEDWINDOW,			// WS : window style
+		CW_USEDEFAULT,					// CW : create window and use default value
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
